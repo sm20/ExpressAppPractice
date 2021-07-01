12aba1ebc355fad210aeff1b86c2ae2961660936
sm20 <sajidmahmood20@gmail.com>
2021-07-01 15:15:11 -0600

    Link to Invoice created on homepage. Does not lead anywhere yet.

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,7 @@
 const getHomepage = (req, res) =>
     res.status(200).send('<h1>Home Page</h1>'
         + '<a href="/api/products"> Products</a><br>'
-        + '<a href="/api/discounted"> Discounted Products</a>')
+        + '<a href="/api/discounted"> Discounted Products</a><br>'
+        + '<a href="/api/invoice">Create Invoice</a><br>')
 
 module.exports = getHomepage
