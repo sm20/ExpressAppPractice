2afb8ac8dcd44163e6b1ba431777fe8e108bc4b1
sm20 <sajidmahmood20@gmail.com>
2021-07-02 17:11:45 -0600

    Changed index page to remove info not needed

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,8 @@
 const getHomepage = (req, res) =>
-    res.status(200).send('<h1>Home Page</h1>'
+    res.status(200).send('Test Email Service through Postman or something similar...');
+/*    res.status(200).send('<h1>Home Page</h1>'
         + '<a href="/api/products"> Products</a><br>'
         + '<a href="/api/discounted"> Discounted Products</a><br>'
         + '<a href="/api/invoice">Create Invoice</a><br>')
-
+*/
 module.exports = getHomepage
