e99a3f8d5db6b5ee3253502192cfb327b1967440
sm20 <sajidmahmood20@gmail.com>
2021-07-08 04:27:28 -0600

    NEW-- headers and billing info based off of new template

--- controllers/invoice.js
+++ controllers/invoice.js
@@ -7,7 +7,7 @@ var nodemailer = require('nodemailer'); //email with attachments
 const emailInvoice = (req, res) => {
 
     //request payload should include these json keys
-    const { invNum } = req.body;    //invoice number
+ /*   const { invNum } = req.body;    //invoice number
     const { firstName, lastName, addressLine1, addressLine2,    //customer info
         city, provinceOrState, country, postalCode, email } = req.body;
 
@@ -18,31 +18,100 @@ const emailInvoice = (req, res) => {
     } else {
         fullAddress = addressLine1 + ", " + addressLine2;
     }
+*/
+    const { providerProfession, providerFirstName, providerLastName
+        , invoiceNumber, invoiceDate
+        , insurerName, insurerAddrLine1, insurerAddrLine2, insurerPhone, insurerCity, insurerProvince
+        , clientFirstName, clientLastName, clientAddrLine1, clientAddrLine2, clientCity, clientProvince, clientPhone
+         } = req.body;
+
+    //full addresses
+    let insurerFullAddr = "";
+    if (insurerAddrLine2 === "") {
+        insurerFullAddr = insurerAddrLine1 + ', ' + insurerCity + ', ' + insurerProvince;
+    } else {
+        insurerFullAddr = insurerAddrLine1 + ', ' + insurerAddrLine2 + ', ' + insurerCity + ', ' + insurerProvince;
+    }
+
+    let clientFullAddr = "";
+    if (clientAddrLine2 === "") {
+        clientFullAddr = clientAddrLine1 + ', ' + clientCity + ', ' + clientProvince;
+    } else {
+        clientFullAddr = clientAddrLine1 + ', ' + clientAddrLine2 + ', ' + clientCity + ', ' + clientProvince;
+    }
 
     //Current Date
     const today = new Date();
     const date = today.toLocaleDateString();  //formatted
 
     //Create initial PDF file
-    const doc = new PDFDocument({ size: 'LETTER' });    //new instance of PDFDocument class
+    const doc = new PDFDocument({ size: 'LETTER', autoFirstPage: false });    //new instance of PDFDocument class
     doc.pipe(fs.createWriteStream('invoice/invoice.pdf'));  //save pdf file in folder: projectRoot/invoice/
 
 
     //populate PDF file
 
+    doc.addPage({
+        margin: 50
+    });
+
     //header excluding logo
-    doc.moveDown(2);
-    doc.fontSize(32);
-    doc.text('INVOICE', { align: 'right' }).moveDown(0.01)
-        .fontSize(12)
-        .text(`Invoice #: ${invNum}`, { align: 'right' }).moveDown(0.25)
-        .text(`Date: ${date}`, { align: 'right' }).moveDown(0.25)
-        .moveTo(72, 181).lineTo(540, 181).stroke()  //horizontal line
-        .moveDown(3);
+    doc.fontSize(28).font('Times-Roman');
+    doc.text('Growth Empowerment', { align: 'right' }).moveDown(0.01)
+        .fontSize(14)
+        .text('Address: 113 14 Ave NE, Calgary, AB', { align: 'right' }).moveDown(0.25)
+        .text('Phone Number: +14552031089', { align: 'right' }).moveDown(0.25)
+        .text('Email Address: GrowthEmpowerment@deenstrong.com', { align: 'right' }).moveDown(1)
+        .text('Provider Information', { align: 'right' }).moveDown(0.25)
+        .text(`${providerProfession}, ${providerFirstName} ${providerLastName}`, { align: 'right' }).moveDown(1);
+
 
     //logo image
-    doc.image('public/GElogo.jpg', 72, 72, { scale: 0.85 })
+    doc.image('public/GElogo.jpg', 50, 50, { scale: 0.70 });
 
+    //invoice Information
+    doc.fontSize(16).font('Times-Bold')
+        .text('Invoice').moveDown(1)
+        .fontSize(14).font('Times-Roman')
+        .text(`Invoice #: `, { continued: true })
+        .text(`${invoiceNumber}`, { underline: true }) //https://stackoverflow.com/questions/20598693/can-i-mix-font-weights-in-the-same-paragraph-when-using-pdfkit
+        .text(`Invoice Date: `, { continued: true })
+        .text(`${invoiceDate}`, { underline: true })
+        .moveDown();
+
+    //Billing Information
+
+    //columns are being used to display the information here: https://github.com/foliojs/pdfkit/issues/819
+    //column 1
+    let LeftCol1 = 50;  //left margin and starting point for col 1
+    let topCol_1and2 = doc.y;   //left margin and starting point for col 1 and 2
+    const colWidth = (doc.page.width - 100) / 2;  //-100 for margin consideration
+    const leftCol2 = LeftCol1 + colWidth + 10; //col 2 x position including 10 pt buffer
+    const colWidth2 = colWidth - 10;    //-10 to balance out previous +10
+    let maxY = doc.y;   //initial maximum y value
+    //note: track col1col2X, col1Y, col2Y to track columns individually.
+
+
+    let billingInfo = ['Bill To:', 'Client:', `${insurerName}`, `${clientFirstName} ${clientLastName}`
+        , `Address: ${insurerFullAddr}`, `Address: ${clientFullAddr}`
+        , `Phone Number: ${insurerPhone}`, `Phone Number: ${clientPhone}`];
+
+    billingInfo.forEach((value, index) => {
+        index % 2 == 0 ?
+            doc.text(value, LeftCol1, topCol_1and2 = maxY, { width: colWidth }) :
+            doc.text(value, leftCol2, topCol_1and2, { width: colWidth2 });
+        maxY = (doc.y > maxY) ? doc.y : maxY;
+    });
+
+        /*.text()
+        .text()
+        .text()
+        .text()
+        .text()
+        .text()
+        ;*/
+
+/*
     //customer information
     doc.fontSize(14).font('Helvetica-Bold')
         .text('Bill To:').moveDown(0.25)
@@ -56,12 +125,12 @@ const emailInvoice = (req, res) => {
 
     //table column headers
 
-
+*/
     doc.end();
 
 
 
-
+/*
     //Email Invoice
 
     //create email transporter
@@ -97,11 +166,12 @@ const emailInvoice = (req, res) => {
 
 
 
-
+*/
     //response to send back to requester
     res.status(202).json({
         success: true,
-        msg: `Invoice for ${firstName} ${lastName} emailed to ${email}`
+       // msg: `Invoice for ${firstName} ${lastName} emailed to ${email}`
+        msg: `success`
     });
 
 }
